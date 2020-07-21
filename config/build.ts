06d01673354589aeab885494c45fd779470e0555
Alex <hansen.alexander.s@gmail.com>
2020-07-20 21:18:56 -0400

    Use Deno-native file scanning strategy

--- config/build.ts
+++ config/build.ts
@@ -1,53 +1,79 @@
-let getScriptFilesRecursive = undefined;
-let code = 0;
+console.log("Scanning scripts folder for files...");
+const scriptFiles = Deno.readDirSync("scripts/");
+const scriptPaths = resolveScripts(`${Deno.cwd()}/scripts`,scriptFiles);
+let failures = 0;
+const failedCompilations = [];
 
-switch(Deno.build.os){
-    case "linux":
-    case "darwin": // I haven't tested this on darwin arch but... it's unix-like, so... it'll work ...right? .....right?
+function resolveScripts(basePath: string, inputFiles: Iterable<Deno.DirEntry>): string[] {
+    const files = [];
+    const dirs = [];
 
-        // read all .ts files recursively down the scripts/ directory
-        getScriptFilesRecursive = Deno.run({
-            cmd: ["sh", "-c", "find scripts/* | grep .ts$"],
-            stdout: "piped"
-        });
-        break;
-    case "windows":
-
-        // read all .ts files recursively down the scripts/ directory
-        getScriptFilesRecursive = Deno.run({
-            cmd: ["dir", "scripts\\*.ts", "/b"],
-            stdout: "piped"
-        });
-        break;
-}
-if(!!!getScriptFilesRecursive) { console.error("Something hecked up!!"); Deno.exit(1); }
-const fileStat = await getScriptFilesRecursive.status();
-code = fileStat.code;
-
-if(code === 0) {
-    const filesRaw = await getScriptFilesRecursive.output();
-    const files: string[] = new TextDecoder().decode(filesRaw).split("\n");
-    console.log(files.join(":"));
-    for(const file of files) {
-        if(file === "") break;
-        const fileSuffix = file.slice(file.lastIndexOf('/')+1);
-        const filename = fileSuffix.substring(0,fileSuffix.lastIndexOf("."));
-        const buildProcess = Deno.run({
-            cmd: ["deno", "bundle", file, `www/scripts/${filename}.js`]
-        });
-        const result = await buildProcess.status();
-        if(result.success) {
-            console.log(`${file} compiled to www/scripts/${filename}.js` );
-        } else {
-            console.error("Failed to compile",file,"!");
+    for (const file of inputFiles) {
+        if(file.isFile && /.*\.[t|j]s$/.test(file.name)) {
+            files.push(`${basePath}/${file.name}`)
+        } else if(file.isDirectory){
+            dirs.push(file);
         }
     }
-    console.log("\n\n-----------------\n   BUILD SUCCESS\n-----------------");
-    Deno.exit(0);
 
-} else { // Failed to stat scripts/ for .ts files...
-    const rawError = await getScriptFilesRecursive.stderrOutput();
-    const errorString = new TextDecoder().decode(rawError);
-    console.log("Unable to list files! (Did you run with --allow-run and --allow-read ?): ",errorString);
-    Deno.exit(code);
+    // start at each directory in scripts/ and recursively resolve file paths
+    for (const dir of dirs) {
+        const newFiles = resolveDir(`${basePath}/${dir.name}`);
+        for(const newFile of newFiles) {
+            files.push(newFile);
+        }
+    }
+
+    return files;
+}
+
+function resolveDir(path: string): string[] {
+    const localEntries = Deno.readDirSync(path);
+    const localFiles = [];
+    const localDirs = [];
+
+    for (const file of localEntries) {
+        if(file.isFile && /.*\.[t|j]s$/.test(file.name)) {
+            localFiles.push(`${path}/${file.name}`)
+        } else if(file.isDirectory){
+            localDirs.push(file);
+        }
+    }
+
+    for (const dir of localDirs) {
+        const newFiles = resolveDir(`${path}/${dir.name}`);
+        for(const newFile of newFiles) {
+            localFiles.push(newFile);
+        }
+    }
+
+    return localFiles;
+}
+
+for (const file of scriptPaths) {
+    const fileSuffix = file.slice(file.lastIndexOf('/')+1);
+    const filename = fileSuffix.substring(0,fileSuffix.lastIndexOf("."));
+    const buildProcess = Deno.run({
+        cmd: ["deno", "bundle", file, `www/scripts/${filename}.js`]
+    });
+    const result = await buildProcess.status();
+    if(result.success) {
+        console.log(`${file} compiled to www/scripts/${filename}.js` );
+    } else {
+        console.error("Failed to compile",file,"!");
+        failedCompilations.push(file);
+        failures++;
+    }
+}
+
+if (failures > 0) {
+    console.error("\n\n-------------------\n   BUILD FAILED\n-------------------");
+    console.warn(
+        "The build failed because the following files failed to compile!: [",
+        failedCompilations.join(', '),
+        "]");
+    Deno.exit(1);
+} else {
+    console.log("\n\n-------------------\n   BUILD SUCCESS\n-------------------");
+    Deno.exit(0);
 }
--- scripts/nested/test3.ts
+++
@@ -1,3 +0,0 @@
-export class Banana {
-    whatColor() { console.log("Yellow is best!"); }
-}
